2440ebcf83562f79bdf19c879426de8bff8552fc
Mingzhe <m.shi@student.tue.nl>
2022-09-03 00:39:32 +0200

    #test: `nacos` remote call using `openFeign`

--- gulimall-member/src/main/java/com/rocs/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/rocs/gulimall/member/GulimallMemberApplication.java
@@ -3,9 +3,18 @@ package com.rocs.gulimall.member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * 1、希望远程调用别的服务
+ * 1）引入openFeign
+ * 2） 编写一个接口告诉SpringCloud这个接口需要调用远程服务
+ *  - 声明接口的每个方法需要调用远程服务的哪个请求
+ *  - 开启远程调用功能
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.rocs.gulimall.member.feign")
 public class GulimallMemberApplication {
 
     public static void main(String[] args) {
---
+++ gulimall-member/src/main/java/com/rocs/gulimall/member/feign/CouponFeignService.java
@@ -0,0 +1,12 @@
+package com.rocs.gulimall.member.feign;
+
+import com.rocs.common.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("gulimall-coupon")
+public interface CouponFeignService {
+
+    @RequestMapping("/coupon/coupon/member/list")
+    public R memberCoupons();
+}
