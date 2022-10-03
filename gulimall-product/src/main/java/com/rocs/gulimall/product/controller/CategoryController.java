6258871d989e67874b4c5bced7f61514dc27f566
Mingzhe <m.shi@student.tue.nl>
2022-10-03 18:27:49 +0200

    #finish: dragging menu

--- gulimall-product/src/main/java/com/rocs/gulimall/product/controller/CategoryController.java
+++ gulimall-product/src/main/java/com/rocs/gulimall/product/controller/CategoryController.java
@@ -53,7 +53,7 @@ public class CategoryController {
     public R info(@PathVariable("catId") Long catId){
 		CategoryEntity category = categoryService.getById(catId);
 
-        return R.ok().put("category", category);
+        return R.ok().put("data", category);
     }
 
     /**
@@ -64,6 +64,16 @@ public class CategoryController {
     public R save(@RequestBody CategoryEntity category){
 		categoryService.save(category);
 
+        return R.ok();
+    }
+
+    /**
+     * 批量修改
+     */
+    @RequestMapping("/update/sort")
+    //@RequiresPermissions("product:category:update")
+    public R update(@RequestBody CategoryEntity[] category){
+        categoryService.updateBatchById(Arrays.asList(category));
         return R.ok();
     }
 
